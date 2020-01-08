73f86498ff551b03e7b5432a30afbf9c2d7e48fc
naheed2510 <you@example.com>
2020-01-08 14:31:35 +0530

    Done

---
+++ first.py
@@ -0,0 +1,34 @@
+print"......comment......"
+print'single line  ' "#"
+print 'multi line("---")'
+print"......data types......"
+a=16 #int datatype
+a1="abc" #string datatypes
+b=123.25 #float datatypes
+e=a+b #expression
+v= 2.3+3.2j #complex datatype
+print"......expression......"
+c=10
+d=20
+e=c+d
+f=d-c
+print "sum is",e
+print "subraction is",f
+f=float(a)
+print "int to float",f
+h=oct(d)
+print "decimal to oct",h
+m=bin(d)
+print"decimal to binary", m
+n=hex(d)
+print"decimal to hex", n
+print ".....i/o....."
+a=raw_input("  enter your name\n")
+b=raw_input(" enter your age\n")
+c=raw_input(" enter your year\n")
+print (".... details....")
+print   a
+print   b
+print   c
+
+
